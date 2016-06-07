37f382b6d2270c97f7f1f5a3fd4ddc23c5ea49e6
krisrybak <kris.rybak@hotmail.co.uk>
2016-06-07 08:51:40 +0100

    Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RybakDigital\Bundle\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RybakDigitalUserBundle:Default:index.html.twig');
+    }
+}
---
+++ RybakDigitalUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RybakDigital\Bundle\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RybakDigitalUserBundle extends Bundle
+{
+}
