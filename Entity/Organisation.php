e34a7bed34acc902298fd8285af2b5b2f6d24481
krisrybak <kris.rybak@hotmail.co.uk>
2017-01-31 10:33:47 +0000

    Adds Organisation entity

---
+++ Entity/Organisation.php
@@ -0,0 +1,275 @@
+<?php
+
+namespace RybakDigital\Bundle\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\Response;
+use RybakDigital\Bundle\UserBundle\Entity\User;
+use RybakDigital\Bundle\UserBundle\Entity\Traits\OrganisationUser;
+
+/**
+ * RybakDigital\Bundle\UserBundle\Entity\Organisation
+ *
+ * @author Kris Rybak <kris.rybak@krisrybak.com>
+ * @ORM\Table(name="acl_organisations")
+ * @ORM\Entity(repositoryClass="RybakDigital\Bundle\UserBundle\Entity\OrganisationRepository")
+ */
+class Organisation
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Organisation", mappedBy="parents")
+     */
+    private $children;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Organisation", inversedBy="children")
+     * @ORM\JoinTable(name="acl_organisation_parents",
+     *      joinColumns={@ORM\JoinColumn(name="organisation_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="parent_id", referencedColumnName="id")}
+     * )
+     */
+    private $parents;
+
+    /**
+     * @var     array
+     */
+    private $descendants;
+
+    /**
+     * @var     array
+     */
+    private $ancestors;
+
+    public function __construct() {
+        $this->children     = new ArrayCollection();
+        $this->parents      = new ArrayCollection();
+        $this->descendants  = array();
+        $this->ancestors    = array();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set id
+     *
+     * @param   integer     $id
+     * @return  Organisation
+     * @throws  InvalidArgumentException
+     */
+    public function setId($id)
+    {
+        if (is_integer($id)) {
+            $this->id = $id;
+        } else {
+            throw new \InvalidArgumentException("Organisation id must be an integer", Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return  string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param   string $name
+     * @return  Organisation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return  string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set description
+     *
+     * @param   string $description
+     * @return  Organisation
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get parents
+     *
+     * @return  ArrayCollection
+     */
+    public function getParents()
+    {
+        return $this->parents;
+    }
+
+    /**
+     * Add parent
+     *
+     * @return  Organisation
+     */
+    public function addParent(Organisation $parent)
+    {
+        $this->parents[] = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Remove parent
+     *
+     * @return  Organisation
+     */
+    public function removeParent(Organisation $parent)
+    {
+        $this->parents->removeElement($parent);
+
+        return $this;
+    }
+
+    /**
+     * Get children
+     *
+     * @return  ArrayCollection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Add child
+     *
+     * @return  Organisation
+     */
+    public function addChild(Organisation $child)
+    {
+        $this->children[] = $child;
+
+        return $this;
+    }
+
+    /**
+     * Remove child
+     *
+     * @return  Organisation
+     */
+    public function removeChild(Organisation $child)
+    {
+        $this->children->removeElement($child);
+
+        return $this;
+    }
+
+    /**
+     * Get descendants
+     *
+     * @return  array
+     */
+    public function getDescendants()
+    {
+        foreach ($this->getChildren()->toArray() as $organisation) {
+            $this->descendants[$organisation->getId()] = $organisation;
+            $this->addOrganisationInheritedChildren($organisation);
+        }
+
+        return $this->descendants;
+    }
+
+    /**
+     * Get inherited children
+     *
+     * @return  void()
+     */
+    public function addOrganisationInheritedChildren(Organisation $organisation)
+    {
+        foreach ($descendants = $organisation->getChildren() as $descendant) {
+            if ($descendant) {
+                $this->descendants[$descendant->getId()] = $descendant;
+
+                if ($descendant->getChildren()->count() > 0) {
+                    $this->addOrganisationInheritedChildren($descendant);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get ancestors
+     *
+     * @return  array
+     */
+    public function getAncestors()
+    {
+        foreach ($this->getParents()->toArray() as $organisation) {
+            $this->ancestors[$organisation->getId()] = $organisation;
+            $this->addOrganisationInheritedParents($organisation);
+        }
+
+        return $this->ancestors;
+    }
+
+    /**
+     * Get inherited parents
+     *
+     * @return  void()
+     */
+    public function addOrganisationInheritedParents(Organisation $organisation)
+    {
+        foreach ($ancestors = $organisation->getParents() as $ancestor) {
+            if ($ancestor) {
+                $this->ancestors[$ancestor->getId()] = $ancestor;
+
+                if ($ancestor->getParents()->count() > 0) {
+                    $this->addOrganisationInheritedParents($ancestor);
+                }
+            }
+        }
+    }
+}
