688f83bb40938525cf111292a3a87e0a657a1031
krisrybak <kris.rybak@hotmail.co.uk>
2017-02-24 10:57:50 +0000

    Adds doctrine migration

---
+++ DoctrineMigrations/Version20170224105650.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RybakDigital\UserBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170224105650 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE acl_organisations ADD created_at DATETIME DEFAULT NULL, CHANGE name name VARCHAR(128) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE acl_organisations DROP created_at, CHANGE name name VARCHAR(64) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
