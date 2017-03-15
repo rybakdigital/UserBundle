41c3548eda9c5aab7beef677529fb23341b00468
krisrybak <kris.rybak@hotmail.co.uk>
2017-03-15 11:39:32 +0000

    Adds doctrine migration

---
+++ DoctrineMigrations/Version20170315113848.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace RybakDigital\UserBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170315113848 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE acl_organisations ADD namespace VARCHAR(128) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_83E1D8C733E16B56 ON acl_organisations (namespace)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_83E1D8C733E16B56 ON acl_organisations');
+        $this->addSql('ALTER TABLE acl_organisations DROP namespace');
+    }
+}
